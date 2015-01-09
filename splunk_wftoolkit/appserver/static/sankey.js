24849a7aa70da76145d5ac45dee06e463095c02b
magnew <magnew@magnew-mbpr.local>
2015-01-09 12:39:36 -0800

    Examples doc and viz pages without Django

---
+++ splunk_wftoolkit/appserver/static/sankey.js
@@ -0,0 +1,49 @@
+define([
+    'jquery', 
+    'underscore', 
+    'prettify', 
+    'splunkjs/ready!', 
+    'text!splunk_wftoolkit/vizpagetemplate.tmpl',
+    'text!splunk_wftoolkit/sankeyCodeSample.txt',
+    'text!splunk_wftoolkit/sankeyUsage.html',
+    'splunkjs/mvc/searchmanager',
+    'splunk_wftoolkit/components/sankey/sankey'
+    ], 
+    function(
+        $, 
+        _, 
+        prettyPrint,
+        mvc,
+        VizTemplate,
+        CodeSample,
+        Usage,
+        SearchManager,
+        SankeyChart
+    ) {
+        return {
+            render: function() {
+                var pageContent = _.template(
+                    VizTemplate, 
+                    {
+                        heading: 'Sankey Chart', 
+                        usage: Usage, 
+                        codesample: CodeSample
+                    }
+                );
+                $('#wide-content').append(pageContent);
+                prettyPrint();  
+
+                new SearchManager({
+                    id: 'sankey-search',
+                    search: '| inputlookup faa.demo.csv | search DestCityName=* OriginCityName=* AND DestCityName!="" AND OriginCityName!="" | head 100 | stats count by OriginCityName DestCityName'
+                });
+
+                new SankeyChart({
+                    id: 'sankey-chart',
+                    managerid: 'sankey-search',
+                    el: $('#example')
+                }).render();
+            }
+        }
+    }
+);
