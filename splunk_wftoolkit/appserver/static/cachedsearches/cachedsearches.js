9adf106063d1bbf6a7ba42b5d900f0da10ecc81c
Siegfried Puchbauer <spuchbauer@splunk.com>
2015-06-19 19:11:48 -0700

    Simplified and updated cachedsearches example

--- splunk_wftoolkit/appserver/static/cachedsearches/cachedsearches.js
+++ splunk_wftoolkit/appserver/static/cachedsearches/cachedsearches.js
@@ -1,4 +1,4 @@
-define([
+require([
     'jquery', 
     'underscore', 
     'splunkjs/ready!', 
@@ -14,45 +14,41 @@ define([
         PostProcessManager,
         TableView
     ) {
-        return {
-            render: function() {
 
-                new SearchManager({
-                    id: 'nocache',
-                    search: 'index=_internal | head 1000 | stats count by sourcetype',
-                    cache: false,
-                });
+        new SearchManager({
+            id: 'nocache',
+            search: 'index=_internal | head 1000 | stats count by sourcetype',
+            cache: false,
+        });
 
-                new SearchManager({
-                    id: 'cache',
-                    search: 'index=_internal | head 1000 | stats count by sourcetype',
-                    cache: true,
-                });
+        new SearchManager({
+            id: 'cache',
+            search: 'index=_internal | head 1000 | stats count by sourcetype',
+            cache: true,
+        });
 
-                new SearchManager({
-                    id: 'cache60',
-                    search: 'index=_internal | head 1000 | stats count by sourcetype',
-                    cache: 60,
-                });
-                
-                new TableView({
-                    id: 'table1',
-                    managerid: 'nocache',
-                    el: $('#table1')
-                }).render();
-                
-                new TableView({
-                    id: 'table2',
-                    managerid: 'cache',
-                    el: $('#table2')
-                }).render();
+        new SearchManager({
+            id: 'cache60',
+            search: 'index=_internal | head 1000 | stats count by sourcetype',
+            cache: 60,
+        });
+        
+        new TableView({
+            id: 'table1',
+            managerid: 'nocache',
+            el: $('#table1')
+        }).render();
+        
+        new TableView({
+            id: 'table2',
+            managerid: 'cache',
+            el: $('#table2')
+        }).render();
 
-                new TableView({
-                    id: 'table3',
-                    managerid: 'cache60',
-                    el: $('#table3')
-                }).render();
-            }
-        }
+        new TableView({
+            id: 'table3',
+            managerid: 'cache60',
+            el: $('#table3')
+        }).render();
     }
 );
