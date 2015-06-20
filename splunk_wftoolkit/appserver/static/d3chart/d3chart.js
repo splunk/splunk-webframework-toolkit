39d33845dc64c4ccd88e3a9405af4770ba657927
Siegfried Puchbauer <spuchbauer@splunk.com>
2015-06-19 19:54:10 -0700

    Simplified and updated d3chart example

--- splunk_wftoolkit/appserver/static/d3chart/d3chart.js
+++ splunk_wftoolkit/appserver/static/d3chart/d3chart.js
@@ -1,4 +1,4 @@
-define([
+require([
     'jquery', 
     'underscore', 
     'splunkjs/ready!', 
@@ -12,45 +12,41 @@ define([
         SearchManager,
         D3ChartView
     ) {
-        return {
-            render: function() {
-                new SearchManager({
-                    'id': "search2",
-                    'preview': true,
-                    'search': "|inputlookup faa.demo.csv | search DestCityName=* | stats count by DestCityName | sort - count | head 10",
-                });
+        new SearchManager({
+            'id': "search2",
+            'preview': true,
+            'search': "|inputlookup faa.demo.csv | search DestCityName=* | stats count by DestCityName | sort - count | head 10",
+        });
 
-                new SearchManager({
-                    'id': "search3",
-                    'preview': true,
-                    'search': '|inputlookup faa.demo.csv | head 100000 | search DestCityName="Atlanta, GA" AND DayOfWeek=1 | timechart span=1h count',
-                });
+        new SearchManager({
+            'id': "search3",
+            'preview': true,
+            'search': '|inputlookup faa.demo.csv | head 100000 | search DestCityName="Atlanta, GA" AND DayOfWeek=1 | timechart span=1h count',
+        });
 
-                var bar_chart = new D3ChartView({
-                    "id": "bar",
-                    "managerid": "search2",
-                    "type": "discreteBarChart",
-                    "el": $('#hook')
-                }).render();
+        var bar_chart = new D3ChartView({
+            "id": "bar",
+            "managerid": "search2",
+            "type": "discreteBarChart",
+            "el": $('#hook')
+        }).render();
 
-                bar_chart.settings.set("setup", function(chart){
-                    chart.color(d3.scale.category10().range());
-                    chart.staggerLabels(true);
-                    chart.showValues(true);
-                });
+        bar_chart.settings.set("setup", function(chart){
+            chart.color(d3.scale.category10().range());
+            chart.staggerLabels(true);
+            chart.showValues(true);
+        });
 
-                var donut_chart = new D3ChartView({
-                    "id": "donut",
-                    "managerid": "search2",
-                    "type": "pieChart",
-                    "el": $('#hook1')
-                }).render();
+        var donut_chart = new D3ChartView({
+            "id": "donut",
+            "managerid": "search2",
+            "type": "pieChart",
+            "el": $('#hook1')
+        }).render();
 
-                donut_chart.settings.set("setup", function(chart){
-                    chart.color(d3.scale.category10().range());
-                    chart.donut(true);
-                });
-            }
-        }
+        donut_chart.settings.set("setup", function(chart){
+            chart.color(d3.scale.category10().range());
+            chart.donut(true);
+        });
     }
 );
