8111a34a5a6e2dd169c66a31b1012f99a5d7e169
Siegfried Puchbauer <spuchbauer@splunk.com>
2015-06-19 23:50:56 -0700

    Simplified and updated jsonresultsviewer example

--- splunk_wftoolkit/appserver/static/jsonresultsviewer/jsonresultsviewer.js
+++ splunk_wftoolkit/appserver/static/jsonresultsviewer/jsonresultsviewer.js
@@ -1,4 +1,4 @@
-define([
+require([
     'jquery', 
     'underscore', 
     'splunkjs/ready!', 
@@ -14,34 +14,30 @@ define([
         SearchbarView,
         ResultsView
     ) {
-        return {
-            render: function() {
 
-                var search1 = new SearchManager({
-                    id: 'search1',
-                    search: 'index=_internal | head 10',
-                });
+        var search1 = new SearchManager({
+            id: 'search1',
+            search: 'index=_internal | head 10',
+        });
 
-                var searchbar1 = new SearchbarView({
-                    id: 'searchbar1',
-                    managerid: 'search1',
-                    el: $('#searchbar1')
-                }).render();
+        var searchbar1 = new SearchbarView({
+            id: 'searchbar1',
+            managerid: 'search1',
+            el: $('#searchbar1')
+        }).render();
 
-                var resultsView = new ResultsView({
-                    id: 'resultsview',
-                    managerid: 'search1',
-                    number: 10,
-                    el: $('#resultsview')
-                }).render();
+        var resultsView = new ResultsView({
+            id: 'resultsview',
+            managerid: 'search1',
+            number: 10,
+            el: $('#resultsview')
+        }).render();
 
-                searchbar1.on('change', function() { 
-                  search1.set('search', searchbar1.val());
-                });
-                $('#console-spill').on('click', function(){
-                    console.log(resultsView.getData());
-                });   
-            }
-        }
+        searchbar1.on('change', function() { 
+          search1.set('search', searchbar1.val());
+        });
+        $('#console-spill').on('click', function(){
+            console.log(resultsView.getData());
+        });   
     }
 );
