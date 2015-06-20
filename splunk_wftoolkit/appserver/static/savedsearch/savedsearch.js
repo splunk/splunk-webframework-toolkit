cb832cb7f8ebae19257c49d60ba89b43443017e9
Siegfried Puchbauer <spuchbauer@splunk.com>
2015-06-19 19:01:41 -0700

    Simplified and updated savedsearch example

--- splunk_wftoolkit/appserver/static/savedsearch/savedsearch.js
+++ splunk_wftoolkit/appserver/static/savedsearch/savedsearch.js
@@ -1,4 +1,4 @@
-define([
+require([
     'jquery', 
     'underscore', 
     'splunkjs/ready!', 
@@ -14,27 +14,23 @@ define([
         TableView,
         ChartView
     ) {
-        return {
-            render: function() {
-                new SavedSearchManager({
-                    id: 'savedsearch1',
-                    searchname: 'Top five sourcetypes',
-                    app: 'search'
-                });
+        new SavedSearchManager({
+            id: 'savedsearch1',
+            searchname: 'Top five sourcetypes',
+            app: 'search'
+        });
 
-                new TableView({
-                    id: 'table1',
-                    managerid: 'savedsearch1',
-                    el: $('#table1')
-                }).render();
+        new TableView({
+            id: 'table1',
+            managerid: 'savedsearch1',
+            el: $('#table1')
+        }).render();
 
-                new ChartView({
-                    id: 'chart1',
-                    managerid: 'savedsearch1',
-                    type: 'pie',
-                    el: $('#chart1')
-                }).render();
-            }
-        }
+        new ChartView({
+            id: 'chart1',
+            managerid: 'savedsearch1',
+            type: 'pie',
+            el: $('#chart1')
+        }).render();
     }
 );
