20c1444799d3eb6d0fcaa34880688d9d5654e9de
Siegfried Puchbauer <spuchbauer@splunk.com>
2015-06-19 18:51:28 -0700

    Simplified and updated parallelcoordinates example

--- splunk_wftoolkit/appserver/static/parallelcoordinates/parallelcoordinates.js
+++ splunk_wftoolkit/appserver/static/parallelcoordinates/parallelcoordinates.js
@@ -1,10 +1,10 @@
-define([
+require([
     'jquery', 
     'underscore', 
     'splunkjs/ready!', 
     'text!splunk_wftoolkit/vizpagetemplate.tmpl',
-    'text!splunk_wftoolkit/parallelcoordinates/parallelcoordinatesCodeSample.txt',
-    'text!splunk_wftoolkit/parallelcoordinates/parallelcoordinatesUsage.html',
+    'text!splunk_wftoolkit/parallelcoordinates/sample.txt',
+    'text!splunk_wftoolkit/parallelcoordinates/usage.html',
     'splunkjs/mvc/searchmanager',
     'splunkjs/mvc/dropdownview',
     'splunk_wftoolkit/components/parallelcoords/parallelcoords'
@@ -20,36 +20,32 @@ define([
         DropdownView,
         ParallelCoordinates
     ) {
-        return {
-            render: function() {
-                var pageContent = _.template(
-                    VizTemplate, 
-                    {
-                        heading: 'Parallel Coordinates', 
-                        usage: Usage, 
-                        codesample: CodeSample
-                    }
-                );
-                $('#wide-content').append(pageContent);
-
-                new SearchManager({
-                    id: 'search1',
-                    search: '| inputlookup faa.demo.csv | search DestCityName="*, NY" OR DestCityName="*, CA" OR DestCityName="*, GA" OriginCityName="Seattle, WA" OR OriginCityName="San FranCisco, CA" OR OriginCityName="New York, NY" AirlineDescription=*| head 10000 | fields AirlineDescription OriginCityName DestCityName'
-                });
-
-                var pc = new ParallelCoordinates({
-                    id: 'pc',
-                    managerid: 'search1',
-                    el: $('#example')
-                }).render();
-
-                pc.on("select", function(e) {
-                    console.log("SELECT EVENT");
-                    _.each(e.selected, function(path) {
-                        console.log(_.values(path).join(' -> '));
-                    });
-                });   
+        var pageContent = _.template(
+            VizTemplate, 
+            {
+                heading: 'Parallel Coordinates', 
+                usage: Usage, 
+                codesample: CodeSample
             }
-        }
+        );
+        $('#wide-content').append(pageContent);
+
+        new SearchManager({
+            id: 'search1',
+            search: '| inputlookup faa.demo.csv | search DestCityName="*, NY" OR DestCityName="*, CA" OR DestCityName="*, GA" OriginCityName="Seattle, WA" OR OriginCityName="San FranCisco, CA" OR OriginCityName="New York, NY" AirlineDescription=*| head 10000 | fields AirlineDescription OriginCityName DestCityName'
+        });
+
+        var pc = new ParallelCoordinates({
+            id: 'pc',
+            managerid: 'search1',
+            el: $('#example')
+        }).render();
+
+        pc.on("select", function(e) {
+            console.log("SELECT EVENT");
+            _.each(e.selected, function(path) {
+                console.log(_.values(path).join(' -> '));
+            });
+        });   
     }
 );
