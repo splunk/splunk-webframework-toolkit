54ad71e4929a4437f161303060168f92ec3916be
Siegfried Puchbauer <spuchbauer@splunk.com>
2015-06-19 23:46:15 -0700

    Simplified and updated interactiveviews example

--- splunk_wftoolkit/appserver/static/interactiveviews/interactiveviews.js
+++ splunk_wftoolkit/appserver/static/interactiveviews/interactiveviews.js
@@ -1,4 +1,4 @@
-define([
+require([
     'jquery', 
     'underscore', 
     'splunkjs/ready!', 
@@ -14,50 +14,45 @@ define([
         TableView,
         SingleView
     ) {
-        return {
-            render: function() {
+        var tokens = mvc.Components.getInstance("default");
 
-                var tokens = mvc.Components.getInstance("default");
+        new SearchManager({
+            id: 'simplesearch1',
+            search: '| eventcount summarize=false index=* OR index=_* | table index count',
+            cache: true,
+            preview: true,
+            autostart: true
+        });
 
-                new SearchManager({
-                    id: 'simplesearch1',
-                    search: '| eventcount summarize=false index=* OR index=_* | table index count',
-                    cache: true,
-                    preview: true,
-                    autostart: true
-                });
+        var simpleSearch2 = new SearchManager({
+            id: 'simplesearch2',
+            search: mvc.tokenSafe('| eventcount summarize=false index=$index$ | table count'),
+            cache: true,
+            preview: true,
+            autostart: false
+        });
 
-                var simpleSearch2 = new SearchManager({
-                    id: 'simplesearch2',
-                    search: mvc.tokenSafe('| eventcount summarize=false index=$index$ | table count'),
-                    cache: true,
-                    preview: true,
-                    autostart: false
-                });
+        var table1 = new TableView({
+            id: 'table1',
+            managerid: 'simplesearch1',
+            el: $('#table1')
+        }).render();
 
-                var table1 = new TableView({
-                    id: 'table1',
-                    managerid: 'simplesearch1',
-                    el: $('#table1')
-                }).render();
+        var single1 = new SingleView({
+            id: 'single1',
+            managerid: 'simplesearch2',
+            beforeLabel: 'Event Count: ',
+            el: $('#single1')
+        }).render();
 
-                var single1 = new SingleView({
-                    id: 'single1',
-                    managerid: 'simplesearch2',
-                    beforeLabel: 'Event Count: ',
-                    el: $('#single1')
-                }).render();
-
-                // Set the index whenever the table is click
-                table1.on("click:row", function(e) {
-                    e.preventDefault();
-                    
-                    var index = e.model.get("index");
-                    
-                    tokens.set("index", index);
-                    simpleSearch2.startSearch();
-                });  
-            }
-        }
+        // Set the index whenever the table is click
+        table1.on("click:row", function(e) {
+            e.preventDefault();
+            // Grab field value from clicked row
+            var index = e.data['row.index'];
+            // Update token value
+            tokens.set("index", index);
+            simpleSearch2.startSearch();
+        });  
     }
 );
