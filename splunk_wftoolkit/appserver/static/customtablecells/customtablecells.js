1e8ad85dea79ef22cf66f24ea0bb0d34dbb57b7a
Siegfried Puchbauer <spuchbauer@splunk.com>
2015-06-19 19:51:53 -0700

    Simplified and updated customtablecells example

--- splunk_wftoolkit/appserver/static/customtablecells/customtablecells.js
+++ splunk_wftoolkit/appserver/static/customtablecells/customtablecells.js
@@ -1,4 +1,4 @@
-define([
+require([
     'jquery', 
     'underscore', 
     'splunkjs/ready!', 
@@ -12,57 +12,53 @@ define([
         SearchManager,
         TableView
     ) {
-        return {
-            render: function() {
 
-                var tokens = mvc.Components.getInstance("default");
+        var tokens = mvc.Components.getInstance("default");
 
-                new SearchManager({
-                    id: 'simplesearch1',
-                    search: 'index=_internal | head 10000 | stats count by sourcetype,source,host | rangemap field=count low=0-100 elevated=101-1000 default=severe',
-                    cache: true,
-                    preview: true,
-                    autostart: true
-                });
+        new SearchManager({
+            id: 'simplesearch1',
+            search: 'index=_internal | head 10000 | stats count by sourcetype,source,host | rangemap field=count low=0-100 elevated=101-1000 default=severe',
+            cache: true,
+            preview: true,
+            autostart: true
+        });
 
-                var tableView1 = new TableView({
-                    id: 'table1',
-                    managerid: 'simplesearch1',
-                    el: $('#table1')
-                }).render();
+        var tableView1 = new TableView({
+            id: 'table1',
+            managerid: 'simplesearch1',
+            el: $('#table1')
+        }).render();
 
-                var table2 = new TableView({
-                    id: 'table2',
-                    managerid: 'simplesearch1',
-                    el: $('#table2')
-                }).render();
+        var table2 = new TableView({
+            id: 'table2',
+            managerid: 'simplesearch1',
+            el: $('#table2')
+        }).render();
 
-                var ICONS = {
-                    severe: 'alert-circle',
-                    elevated: 'alert',
-                    low: 'check-circle'
-                };
+        var ICONS = {
+            severe: 'alert-circle',
+            elevated: 'alert',
+            low: 'check-circle'
+        };
 
-                var CustomIconCellRenderer = TableView.BaseCellRenderer.extend({
-                    canRender: function(cell) {
-                        return cell.field === 'range';
-                    },
-                    
-                    render: function($td, cell) {
-                        var icon = 'question';
-                        if(ICONS.hasOwnProperty(cell.value)) {
-                            icon = ICONS[cell.value];
-                        }
-                        $td.addClass('icon').html(_.template('<i class="icon-<%-icon%> <%- range %>" title="<%- range %>"></i>', {
-                            icon: icon,
-                            range: cell.value
-                        }));
-                    }
-                });
-
-                tableView1.table.addCellRenderer(new CustomIconCellRenderer());
-                tableView1.table.render();   
+        var CustomIconCellRenderer = TableView.BaseCellRenderer.extend({
+            canRender: function(cell) {
+                return cell.field === 'range';
+            },
+            
+            render: function($td, cell) {
+                var icon = 'question';
+                if(ICONS.hasOwnProperty(cell.value)) {
+                    icon = ICONS[cell.value];
+                }
+                $td.addClass('icon').html(_.template('<i class="icon-<%-icon%> <%- range %>" title="<%- range %>"></i>', {
+                    icon: icon,
+                    range: cell.value
+                }));
             }
-        }
+        });
+
+        tableView1.table.addCellRenderer(new CustomIconCellRenderer());
+        tableView1.table.render();   
     }
 );
