c3a228284a58322b72d530edae5f95e83176a60d
Siegfried Puchbauer <spuchbauer@splunk.com>
2015-06-19 19:13:08 -0700

    Simplified and updated databindingjs example

--- splunk_wftoolkit/appserver/static/databindingjs/databindingjs.js
+++ splunk_wftoolkit/appserver/static/databindingjs/databindingjs.js
@@ -1,4 +1,4 @@
-define([
+require([
     'jquery', 
     'underscore', 
     'splunkjs/ready!', 
@@ -12,25 +12,21 @@ define([
         SearchManager,
         TextInputView
     ) {
-        return {
-            render: function() {
-                // Instantiate components
-                new TextInputView({
-                    id: "leftField",
-                    el: $("#leftField"),
-                    default: "Bob Waters",
-                    value: mvc.tokenSafe("$fullName$")
-                }).render();
+        // Instantiate components
+        new TextInputView({
+            id: "leftField",
+            el: $("#leftField"),
+            default: "Bob Waters",
+            value: mvc.tokenSafe("$fullName$")
+        }).render();
 
-                new TextInputView({
-                    id: "rightField",
-                    el: $("#rightField"),
-                    // NOTE: If this default is specified, it will override the
-                    //       default defined in the view above.
-                    //default: "$1.00 $2.00", // literal value
-                    value: mvc.tokenSafe("$fullName$")
-                }).render();   
-            }
-        }
+        new TextInputView({
+            id: "rightField",
+            el: $("#rightField"),
+            // NOTE: If this default is specified, it will override the
+            //       default defined in the view above.
+            //default: "$1.00 $2.00", // literal value
+            value: mvc.tokenSafe("$fullName$")
+        }).render();   
     }
 );
