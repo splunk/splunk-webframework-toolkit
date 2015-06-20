c4afe5b986a9d5e757d778a1d721573263e3afc3
Siegfried Puchbauer <spuchbauer@splunk.com>
2015-06-20 00:03:58 -0700

    Simplified and updated searchtimeline example

--- splunk_wftoolkit/appserver/static/searchtimeline/searchtimeline.js
+++ splunk_wftoolkit/appserver/static/searchtimeline/searchtimeline.js
@@ -1,4 +1,4 @@
-define([
+require([
     'jquery', 
     'underscore', 
     'splunkjs/ready!', 
@@ -18,50 +18,46 @@ define([
         EventsViewerView,
         SearchbarView
     ) {
-        return {
-            render: function() {
 
-                var defaultNamespace = mvc.Components.getInstance('default'); 
+        var defaultNamespace = mvc.Components.getInstance('default'); 
 
-                new SearchManager({
-                    id: 'main-search',
-                    search: mvc.tokenSafe('$search$'),
-                    app: 'search',
-                    preview: true,
-                    required_field_list: '*',
-                    status_buckets: 300,
-                    earliest_time: mvc.tokenSafe('$timeRange.earliest_time$'),
-                    latest_time: mvc.tokenSafe('$timeRange.latest_time$')
-                });
+        new SearchManager({
+            id: 'main-search',
+            search: mvc.tokenSafe('$search$'),
+            app: 'search',
+            preview: true,
+            required_field_list: '*',
+            status_buckets: 300,
+            earliest_time: mvc.tokenSafe('$timeRange.earliest_time$'),
+            latest_time: mvc.tokenSafe('$timeRange.latest_time$')
+        });
 
-                new SearchbarView({
-                    id: 'main-searchbar',
-                    managerid: 'main-search',
-                    value: mvc.tokenSafe('$search$'),
-                    timerange_value: mvc.tokenSafe('$timeRange$'),
-                    el: $('#main-searchbar')
-                }).render();
+        new SearchbarView({
+            id: 'main-searchbar',
+            managerid: 'main-search',
+            value: mvc.tokenSafe('$search$'),
+            timerange_value: mvc.tokenSafe('$timeRange$'),
+            el: $('#main-searchbar')
+        }).render();
 
-                new SearchControlsView({
-                    id: 'main-searchcontrols',
-                    managerid: 'main-search',
-                    el: $('#main-searchcontrols')
-                }).render();
+        new SearchControlsView({
+            id: 'main-searchcontrols',
+            managerid: 'main-search',
+            el: $('#main-searchcontrols')
+        }).render();
 
-                new TimelineView({
-                    id: 'main-timeline',
-                    managerid: 'main-search',
-                    value: mvc.tokenSafe('$timeRange$'),
-                    el: $('#main-timeline')
-                }).render();
+        new TimelineView({
+            id: 'main-timeline',
+            managerid: 'main-search',
+            value: mvc.tokenSafe('$timeRange$'),
+            el: $('#main-timeline')
+        }).render();
 
-                new EventsViewerView({
-                    id: 'main-events',
-                    managerid: 'main-search',
-                    drilldownRedirect: true,
-                    el: $('#main-events')
-                }).render();
-            }   
-        }
+        new EventsViewerView({
+            id: 'main-events',
+            managerid: 'main-search',
+            drilldownRedirect: true,
+            el: $('#main-events')
+        }).render();
     }
 );
