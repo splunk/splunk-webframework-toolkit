a234cabd1c099a7bd2a595a49c0137ed60d1c3cd
Siegfried Puchbauer <spuchbauer@splunk.com>
2015-06-19 19:16:58 -0700

    Simplified and updated cascading inputs example

--- splunk_wftoolkit/appserver/static/cascadinginputs/cascadinginputs.js
+++ splunk_wftoolkit/appserver/static/cascadinginputs/cascadinginputs.js
@@ -1,4 +1,4 @@
-define([
+require([
     'jquery', 
     'underscore', 
     'splunkjs/ready!', 
@@ -14,87 +14,83 @@ define([
         SingleView,
         DropdownView
     ) {
-        return {
-            render: function() {
 
-                var tokens = mvc.Components.getInstance("default");
+        var tokens = mvc.Components.getInstance("default");
 
-                var simpleSearch1 = new SearchManager({
-                    id: 'simplesearch1',
-                    search: mvc.tokenSafe('index=$index$ sourcetype=$sourcetype$ earliest=-5m | stats count'),
-                    preview: true,
-                    cache: 60,
-                    status_buckets: 300,
-                    autostart: false
-                });
+        var simpleSearch1 = new SearchManager({
+            id: 'simplesearch1',
+            search: mvc.tokenSafe('index=$index$ sourcetype=$sourcetype$ earliest=-5m | stats count'),
+            preview: true,
+            cache: 60,
+            status_buckets: 300,
+            autostart: false
+        });
 
-                new SearchManager({
-                    id: 'indexsearch',
-                    search: '| eventcount summarize=false index=* OR index=_*',
-                    preview: false,
-                    cache: true,
-                });
+        new SearchManager({
+            id: 'indexsearch',
+            search: '| eventcount summarize=false index=* OR index=_*',
+            preview: false,
+            cache: true,
+        });
 
-                var sourcetypeSearch = new SearchManager({
-                    id: 'sourcetypesearch',
-                    search: mvc.tokenSafe('| metadata index=$index$ OR index=_$index$ type=sourcetypes'),
-                    preview: true,
-                    cache: true,
-                    autostart: false
-                });
+        var sourcetypeSearch = new SearchManager({
+            id: 'sourcetypesearch',
+            search: mvc.tokenSafe('| metadata index=$index$ OR index=_$index$ type=sourcetypes'),
+            preview: true,
+            cache: true,
+            autostart: false
+        });
 
-                var indexes = new DropdownView({
-                    id: 'indexes',
-                    managerid: 'indexsearch',
-                    default: '*',
-                    valueField: 'index',
-                    el: $('#indexes')
-                }).render();
+        var indexes = new DropdownView({
+            id: 'indexes',
+            managerid: 'indexsearch',
+            default: '*',
+            valueField: 'index',
+            el: $('#indexes')
+        }).render();
 
-                var sourcetypes = new DropdownView({
-                    id: 'sourcetypes',
-                    managerid: 'sourcetypesearch',
-                    valueField: 'sourcetype',
-                    el: $('#sourcetypes')
-                }).render();
+        var sourcetypes = new DropdownView({
+            id: 'sourcetypes',
+            managerid: 'sourcetypesearch',
+            valueField: 'sourcetype',
+            el: $('#sourcetypes')
+        }).render();
 
-                var single = new SingleView({
-                    id: 'singlevalue',
-                    managerid: 'simplesearch1',
-                    beforeLabel: 'Event Count: ',
-                    el: $('#singlevalue')
-                }).render();
+        var single = new SingleView({
+            id: 'singlevalue',
+            managerid: 'simplesearch1',
+            beforeLabel: 'Event Count: ',
+            el: $('#singlevalue')
+        }).render();
 
-                // Add "all" choices to
-                indexes.settings.set("choices", [
-                    {value: "*", label: "(all)"}    
-                ]);
-                sourcetypes.settings.set("choices", [
-                    {value: "*", label: "(all)"}    
-                ]);
-                
-                // Set up a handler for when the selection changes
-                indexes.on("change", function() {
-                    if (!indexes.val()) {
-                        return;   
-                    }
-                    
-                    // Set the tokens for both managers
-                    tokens.set("index", indexes.val());
-                    
-                    // Start the searches
-                    sourcetypeSearch.startSearch();
-                });
-                
-                $("#searchbutton").on("click", function() {
-                    tokens.set({
-                        index: indexes.val(),
-                        sourcetype: sourcetypes.val()
-                    });
-                    
-                    simpleSearch1.startSearch();
-                });
+        // Add "all" choices to
+        indexes.settings.set("choices", [
+            {value: "*", label: "(all)"}    
+        ]);
+        sourcetypes.settings.set("choices", [
+            {value: "*", label: "(all)"}    
+        ]);
+        
+        // Set up a handler for when the selection changes
+        indexes.on("change", function() {
+            if (!indexes.val()) {
+                return;   
             }
-        }
+            
+            // Set the tokens for both managers
+            tokens.set("index", indexes.val());
+            
+            // Start the searches
+            sourcetypeSearch.startSearch();
+        });
+        
+        $("#searchbutton").on("click", function() {
+            tokens.set({
+                index: indexes.val(),
+                sourcetype: sourcetypes.val()
+            });
+            
+            simpleSearch1.startSearch();
+        });
     }
 );
