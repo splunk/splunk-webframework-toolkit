f48b938a4c65b9ebb9be47bfd186baf8bbbcac22
Siegfried Puchbauer <spuchbauer@splunk.com>
2015-06-20 00:01:49 -0700

    Simplified and updated punchcardsearch example

--- splunk_wftoolkit/appserver/static/punchcardsearch/punchcardsearch.js
+++ splunk_wftoolkit/appserver/static/punchcardsearch/punchcardsearch.js
@@ -1,4 +1,4 @@
-define([
+require([
     'jquery', 
     'underscore', 
     'splunkjs/ready!', 
@@ -14,43 +14,39 @@ define([
         SearchbarView,
         Punchcard
     ) {
-        return {
-            render: function() {
 
-                var search1 = new SearchManager({
-                    id: 'search1',
-                    search: mvc.tokenSafe('$search$'),
-                });
+        var search1 = new SearchManager({
+            id: 'search1',
+            search: mvc.tokenSafe('$search$'),
+        });
 
-                new SearchbarView({
-                    id: 'searchbar1',
-                    managerid: 'search1',
-                    value: mvc.tokenSafe('$search$'),
-                    default: 'index=_internal | head 300000 | stats count by date_hour sourcetype',
-                    el: $('#searchbar1')
-                }).render();
+        new SearchbarView({
+            id: 'searchbar1',
+            managerid: 'search1',
+            value: mvc.tokenSafe('$search$'),
+            default: 'index=_internal | head 300000 | stats count by date_hour sourcetype',
+            el: $('#searchbar1')
+        }).render();
 
-                
-                var formatHours = function(x) {
-                    var parsed = parseInt(x, 10);
-                    if (x < 12) { 
-                        return (x === 0 ? 12 : x) + "AM";
-                    }
-                    else {
-                        return (x === 12 ? 12 : x-12) + "PM";
-                    }
-                };
-
-                var search1 = mvc.Components.getInstance('search1');
-
-                var pc = new Punchcard({
-                    id: 'pc1',
-                    managerid: 'search1',
-                    formatXAxisLabel: formatHours,
-                    height: 300,
-                    el: $('#hook')
-                }).render();
+        
+        var formatHours = function(x) {
+            var parsed = parseInt(x, 10);
+            if (x < 12) { 
+                return (x === 0 ? 12 : x) + "AM";
             }
-        }
+            else {
+                return (x === 12 ? 12 : x-12) + "PM";
+            }
+        };
+
+        var search1 = mvc.Components.getInstance('search1');
+
+        var pc = new Punchcard({
+            id: 'pc1',
+            managerid: 'search1',
+            formatXAxisLabel: formatHours,
+            height: 300,
+            el: $('#hook')
+        }).render();
     }
 );
