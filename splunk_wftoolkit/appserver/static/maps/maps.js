7276021f3bc2c637f537635b4720e06baa45277d
Siegfried Puchbauer <spuchbauer@splunk.com>
2015-06-19 23:55:06 -0700

    Simplified and updated maps example

--- splunk_wftoolkit/appserver/static/maps/maps.js
+++ splunk_wftoolkit/appserver/static/maps/maps.js
@@ -1,4 +1,4 @@
-define([
+require([
     'jquery', 
     'underscore', 
     'splunkjs/ready!', 
@@ -16,52 +16,41 @@ define([
         MapView,
         ResultsLinkView
     ) {
-        return {
-            render: function() {
 
-                var tokens = mvc.Components.getInstance('default');
+        var tokens = mvc.Components.getInstance('default');
 
-                // new SearchManager({
-                //     id: 'geosearch',
-                //     search: '| inputlookup earthquakes.csv | rename Lat as lat Lon as lon | geostats count',
-                //     cache: true,
-                //     preview: true,  
-                // });
+        new SearchManager({
+            id: 'map-search',
+            search: '| inputlookup earthquakes.csv | rename Lat as lat Lon as lon | geostats count',
+            cache: true,
+            preview: true,  
+        });
 
-                new SearchManager({
-                    id: 'map-search',
-                    search: '| inputlookup earthquakes.csv | rename Lat as lat Lon as lon | geostats count',
-                    cache: true,
-                    preview: true,  
-                });
+        var map1 = new MapView({
+            id: 'map1',
+            managerid: 'map-search',
+            el: $('#map1')
+        }).render();
 
-                var map1 = new MapView({
-                    id: 'map1',
-                    managerid: 'map-search',
-                    el: $('#map1')
-                }).render();
+        var map2 = new MapView({
+            id: 'map2',
+            managerid: 'map-search',
+            tileSource: 'openStreetMap',
+            el: $('#map2')
+        }).render();
 
-                var map2 = new MapView({
-                    id: 'map2',
-                    managerid: 'map-search',
-                    tileSource: 'openStreetMap',
-                    el: $('#map2')
-                }).render();
+        var table1 = new TableView({
+            id: 'table1',
+            managerid: 'map-search',
+            pageSize: 6,
+            fields: ['latitude', 'longitude', 'count'],
+            el: $('#table1')
+        }).render();
 
-                var table1 = new TableView({
-                    id: 'table1',
-                    managerid: 'map-search',
-                    pageSize: 6,
-                    fields: ['latitude', 'longitude', 'count'],
-                    el: $('#table1')
-                }).render();
-
-                var rlv1 = new ResultsLinkView({
-                    id: 'rlv1',
-                    managerid: 'map-search',
-                    el: $('#rlv1')
-                }).render();
-            }
-        }
+        var rlv1 = new ResultsLinkView({
+            id: 'rlv1',
+            managerid: 'map-search',
+            el: $('#rlv1')
+        }).render();
     }
 );
