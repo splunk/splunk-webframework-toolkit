179aaf90ef77ba67ff27d5f1d32f73a2099797ad
Siegfried Puchbauer <spuchbauer@splunk.com>
2015-06-19 19:48:25 -0700

    Simplified and updated chartclicks example

--- splunk_wftoolkit/appserver/static/chartclicks/chartclicks.js
+++ splunk_wftoolkit/appserver/static/chartclicks/chartclicks.js
@@ -1,4 +1,4 @@
-define([
+require([
     'jquery', 
     'underscore', 
     'splunkjs/ready!', 
@@ -14,49 +14,45 @@ define([
         ChartView,
         SingleView
     ) {
-        return {
-            render: function() {
 
-                var tokens = mvc.Components.getInstance("default");
+        var tokens = mvc.Components.getInstance("default");
 
-                new SearchManager({
-                    id: 'simplesearch1',
-                    search: '| metadata type=sourcetypes index=_internal | rename totalCount as count | table sourcetype count',
-                    cache: true,
-                    preview: true,
-                    autostart: true
-                });
+        new SearchManager({
+            id: 'simplesearch1',
+            search: '| metadata type=sourcetypes index=_internal | rename totalCount as count | table sourcetype count',
+            cache: true,
+            preview: true,
+            autostart: true
+        });
 
-                var detailSearch = new SearchManager({
-                    id: 'detailsearch',
-                    search: mvc.tokenSafe('| metadata type=sourcetypes index=_internal | search sourcetype=$sourcetype$'),
-                    cache: true,
-                    preview: true,
-                    autostart: false
-                });
+        var detailSearch = new SearchManager({
+            id: 'detailsearch',
+            search: mvc.tokenSafe('| metadata type=sourcetypes index=_internal | search sourcetype=$sourcetype$'),
+            cache: true,
+            preview: true,
+            autostart: false
+        });
 
-                var sourcetypeChart = new ChartView({
-                    id: 'sourcetype-chart',
-                    managerid: 'simplesearch1',
-                    type: 'pie',
-                    el: $('#sourcetype-chart')
-                }).render();
+        var sourcetypeChart = new ChartView({
+            id: 'sourcetype-chart',
+            managerid: 'simplesearch1',
+            type: 'pie',
+            el: $('#sourcetype-chart')
+        }).render();
 
-                var detailSingle = new SingleView({
-                    id: 'detail-single',
-                    managerid: 'detailsearch',
-                    field: 'totalCount',
-                    afterLabel: 'events',
-                    el: $('#detail-single')
-                }).render();
+        var detailSingle = new SingleView({
+            id: 'detail-single',
+            managerid: 'detailsearch',
+            field: 'totalCount',
+            afterLabel: 'events',
+            el: $('#detail-single')
+        }).render();
 
-                sourcetypeChart.on("click:chart", function(e) {
-                    e.preventDefault();
-                    detailSingle.settings.set("beforeLabel", e.value + ":");
-                    tokens.set("sourcetype", e.value);
-                    detailSearch.startSearch();
-                }); 
-            }
-        }
+        sourcetypeChart.on("click:chart", function(e) {
+            e.preventDefault();
+            detailSingle.settings.set("beforeLabel", e.value + ":");
+            tokens.set("sourcetype", e.value);
+            detailSearch.startSearch();
+        }); 
     }
 );
