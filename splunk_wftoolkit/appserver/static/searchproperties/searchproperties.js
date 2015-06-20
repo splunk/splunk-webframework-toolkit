59f0607a6fa05940d883602a455dba356144bca2
Siegfried Puchbauer <spuchbauer@splunk.com>
2015-06-19 19:07:04 -0700

    Simplified and updated searchproperties example

--- splunk_wftoolkit/appserver/static/searchproperties/searchproperties.js
+++ splunk_wftoolkit/appserver/static/searchproperties/searchproperties.js
@@ -1,4 +1,4 @@
-define([
+require([
     'jquery', 
     'underscore', 
     'splunkjs/ready!', 
@@ -14,41 +14,37 @@ define([
         TimelineView,
         RadioGroupView
     ) {
-        return {
-            render: function() {
 
-                var mainSearch = new SearchManager({
-                    id: 'main-search',
-                    search: 'index=_internal earliest=-10m',
-                    preview: true,
-                    cache: 60,
-                    status_buckets: 300
-                });
+        var mainSearch = new SearchManager({
+            id: 'main-search',
+            search: 'index=_internal earliest=-10m',
+            preview: true,
+            cache: 60,
+            status_buckets: 300
+        });
 
-                var radio = new RadioGroupView({
-                    id: 'timeline-radio',
-                    default: 'yes',
-                    el: $('#timeline-radio')
-                }).render();
-                
-                new TimelineView({
-                    id: 'timeline1',
-                    managerid: 'main-search',
-                    el: $('#timeline1')
-                }).render();
+        var radio = new RadioGroupView({
+            id: 'timeline-radio',
+            default: 'yes',
+            el: $('#timeline-radio')
+        }).render();
+        
+        new TimelineView({
+            id: 'timeline1',
+            managerid: 'main-search',
+            el: $('#timeline1')
+        }).render();
 
-                // Add our static choices
-                radio.settings.set("choices", [
-                    {value: "yes", label: "Yes"},    
-                    {value: "no", label: "No"},
-                ]);
-                
-                // Set up a handler for when the selection changes
-                radio.on("change", function() {            
-                    // Set status buckets for the search
-                    mainSearch.search.set("status_buckets", radio.val() === "yes" ? 300 : 0);
-                });   
-            }
-        }
+        // Add our static choices
+        radio.settings.set("choices", [
+            {value: "yes", label: "Yes"},    
+            {value: "no", label: "No"},
+        ]);
+        
+        // Set up a handler for when the selection changes
+        radio.on("change", function() {            
+            // Set status buckets for the search
+            mainSearch.search.set("status_buckets", radio.val() === "yes" ? 300 : 0);
+        });   
     }
 );
