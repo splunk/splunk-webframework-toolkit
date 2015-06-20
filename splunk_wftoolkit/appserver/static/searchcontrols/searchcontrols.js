80729a54d20fc04ecd7ac3b368c4a5083ad9a385
Siegfried Puchbauer <spuchbauer@splunk.com>
2015-06-19 17:05:49 -0700

    Simplified searchcontrols page

--- splunk_wftoolkit/appserver/static/searchcontrols/searchcontrols.js
+++ splunk_wftoolkit/appserver/static/searchcontrols/searchcontrols.js
@@ -1,4 +1,4 @@
-define([
+require([
     'jquery', 
     'underscore', 
     'splunkjs/ready!', 
@@ -8,7 +8,8 @@ define([
     'splunkjs/mvc/searchcontrolsview',
     'splunkjs/mvc/timerangeview',
     'splunkjs/mvc/timelineview',
-    'splunkjs/mvc/singleview'
+    'splunkjs/mvc/singleview',
+    'css!splunk_wftoolkit/searchcontrols/searchcontrols.css'
     ], 
     function(
         $, 
@@ -22,152 +23,148 @@ define([
         TimelineView,
         SingleView
     ) {
-        return {
-            render: function() {
 
-                new SearchManager({
-                    id: 'example-bar-search',
-                    search: 'index=_internal | head 100'
-                });
+        new SearchManager({
+            id: 'example-bar-search',
+            search: 'index=_internal | head 100'
+        });
 
-                new SearchManager({
-                    id: 'example-searchcontrols-search',
-                    search: 'index=_internal | head 100'
-                });
+        new SearchManager({
+            id: 'example-searchcontrols-search',
+            search: 'index=_internal | head 100'
+        });
 
-                new SearchManager({
-                    id: 'timerange-search',
-                    search: 'index=_internal | head 10000 |stats count'
-                });
+        new SearchManager({
+            id: 'timerange-search',
+            search: 'index=_internal | head 10000 |stats count'
+        });
 
-                new SearchManager({
-                    id: 'example-timeline-search',
-                    search: 'index=_internal | head 1000',
-                    preview: true,
-                    status_buckets: 300
-                });
+        new SearchManager({
+            id: 'example-timeline-search',
+            search: 'index=_internal | head 1000',
+            preview: true,
+            status_buckets: 300
+        });
 
-                // Search bar sample
-                new SearchBarView({
-                    id: 'example-search-bar',
-                    managerid: 'example-bar-search',
-                    el: $('#example-search-bar')
-                }).render();
+        // Search bar sample
+        new SearchBarView({
+            id: 'example-search-bar',
+            managerid: 'example-bar-search',
+            el: $('#example-search-bar')
+        }).render();
 
-                new TableView({
-                    id: 'bar-table',
-                    managerid: 'example-bar-search',
-                    el: $('#bar-table')
-                }).render();
+        new TableView({
+            id: 'bar-table',
+            managerid: 'example-bar-search',
+            el: $('#bar-table')
+        }).render();
 
-                // Search controls sample
-                new SearchBarView({
-                    id: 'searchcontrols-searchbar',
-                    managerid: 'example-searchcontrols-search',
-                    el: $('#searchcontrols-searchbar')
-                }).render();
+        // Search controls sample
+        new SearchBarView({
+            id: 'searchcontrols-searchbar',
+            managerid: 'example-searchcontrols-search',
+            el: $('#searchcontrols-searchbar')
+        }).render();
 
-                new SearchControlsView({
-                    id: 'example-searchcontrols',
-                    managerid: 'example-searchcontrols-search',
-                    pageSize: 5,
-                    el: $('#example-searchcontrols')
-                }).render();
+        new SearchControlsView({
+            id: 'example-searchcontrols',
+            managerid: 'example-searchcontrols-search',
+            pageSize: 5,
+            el: $('#example-searchcontrols')
+        }).render();
 
-                new TableView({
-                    id: 'searchcontrols-table',
-                    managerid: 'example-searchcontrols-search',
-                    pageSize: 5,
-                    el: $('#searchcontrols-table')
-                }).render();
+        new TableView({
+            id: 'searchcontrols-table',
+            managerid: 'example-searchcontrols-search',
+            pageSize: 5,
+            el: $('#searchcontrols-table')
+        }).render();
 
-                // Timeline sample
-                new TimerangeView({
-                    id: 'timeline-timerange',
-                    managerid: 'example-timeline-search',
-                    el: $('#timeline-timerange')
-                }).render();
+        // Timeline sample
+        new TimerangeView({
+            id: 'timeline-timerange',
+            managerid: 'example-timeline-search',
+            el: $('#timeline-timerange')
+        }).render();
 
-                new TimelineView({
-                    id: 'example-timeline',
-                    managerid: 'example-timeline-search',
-                    el: $('#example-timeline')
-                }).render();
+        new TimelineView({
+            id: 'example-timeline',
+            managerid: 'example-timeline-search',
+            el: $('#example-timeline')
+        }).render();
 
-                new TableView({
-                    id: 'timeline-table',
-                    managerid: 'example-timeline-search',
-                    pageSize: 5,
-                    el: $('#timeline-table')
-                }).render();
+        new TableView({
+            id: 'timeline-table',
+            managerid: 'example-timeline-search',
+            pageSize: 5,
+            el: $('#timeline-table')
+        }).render();
 
-                // Timerange sample
-                new TimerangeView({
-                    id: 'example-timerange',
-                    managerid: 'timerange-search',
-                    preset: 'Last 24 hours',
-                    el: $('#example-timerange')
-                }).render();
+        // Timerange sample
+        new TimerangeView({
+            id: 'example-timerange',
+            managerid: 'timerange-search',
+            preset: 'Last 24 hours',
+            el: $('#example-timerange')
+        }).render();
 
-                new SingleView({
-                    id: 'timerange-single',
-                    managerid: 'timerange-search',
-                    afterLabel: 'events',
-                    el: $('#timerange-single')
-                }).render();
+        new SingleView({
+            id: 'timerange-single',
+            managerid: 'timerange-search',
+            afterLabel: 'events',
+            el: $('#timerange-single')
+        }).render();
 
-                // Timeline, TimeRange and Table sample
-                (function() {
-                    var manager = splunkjs.mvc.Components.getInstance("example-timeline-search");
-                    var timerange = splunkjs.mvc.Components.getInstance("timeline-timerange");
-                    var timeline = splunkjs.mvc.Components.getInstance("example-timeline");
-                    
-                    timerange.on("change", function() {
-                        manager.search.set(timerange.val());
-                    });
-                    
-                    timeline.on("change", function() {
-                        manager.search.set(timeline.val());
-                    });
-                })();
+        // Timeline, TimeRange and Table sample
+        (function() {
+            var manager = splunkjs.mvc.Components.getInstance("example-timeline-search");
+            var timerange = splunkjs.mvc.Components.getInstance("timeline-timerange");
+            var timeline = splunkjs.mvc.Components.getInstance("example-timeline");
+            
+            timerange.on("change", function() {
+                manager.search.set(timerange.val());
+            });
+            
+            timeline.on("change", function() {
+                manager.search.set(timeline.val());
+            });
+        })();
 
-                // Search controls sample
-                (function() {
-                    var manager = splunkjs.mvc.Components.getInstance("example-searchcontrols-search");
-                    var searchbar = splunkjs.mvc.Components.getInstance("searchcontrols-searchbar");
-                    var searchcontrols = splunkjs.mvc.Components.getInstance("example-searchcontrols");
-                    
-                    searchbar.on("change", function() {
-                        manager.set("search", searchbar.val()); 
-                    });
-                })();
+        // Search controls sample
+        (function() {
+            var manager = splunkjs.mvc.Components.getInstance("example-searchcontrols-search");
+            var searchbar = splunkjs.mvc.Components.getInstance("searchcontrols-searchbar");
+            var searchcontrols = splunkjs.mvc.Components.getInstance("example-searchcontrols");
+            
+            searchbar.on("change", function() {
+                manager.set("search", searchbar.val()); 
+            });
+        })();
 
 
-                // Search bar sample
-                (function() {
-                    var manager = splunkjs.mvc.Components.getInstance("example-bar-search");
-                    var searchbar = splunkjs.mvc.Components.getInstance("example-search-bar");
-                    var timerange = searchbar.timerange;
+        // Search bar sample
+        (function() {
+            var manager = splunkjs.mvc.Components.getInstance("example-bar-search");
+            var searchbar = splunkjs.mvc.Components.getInstance("example-search-bar");
+            var timerange = searchbar.timerange;
 
-                    searchbar.on("change", function() {
-                        manager.set("search", searchbar.val()); 
-                    });
+            searchbar.on("change", function() {
+                manager.set("search", searchbar.val()); 
+            });
 
-                    timerange.on("change", function() {
-                        manager.search.set(timerange.val()); 
-                    });
-                })();
-                
-                // Time range input sample
-                (function() {
-                    var manager = splunkjs.mvc.Components.getInstance("timerange-search");
-                    var timerange = splunkjs.mvc.Components.getInstance("example-timerange")
+            timerange.on("change", function() {
+                manager.search.set(timerange.val()); 
+            });
+        })();
+        
+        // Time range input sample
+        (function() {
+            var manager = splunkjs.mvc.Components.getInstance("timerange-search");
+            var timerange = splunkjs.mvc.Components.getInstance("example-timerange")
 
-                    timerange.on("change", function() {
-                        manager.search.set(timerange.val()); 
-                    });
-                })();              
-            }
-        }
+            timerange.on("change", function() {
+                manager.search.set(timerange.val()); 
+            });
+        })();              
     }
 );
