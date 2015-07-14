72b80383d8afd3c3f0ce340259043ddbc3d6e32f
Siegfried Puchbauer <spuchbauer@splunk.com>
2015-07-14 16:09:21 -0700

    simplified data template example

--- splunk_wftoolkit/appserver/static/dataview/dataview.js
+++ splunk_wftoolkit/appserver/static/dataview/dataview.js
@@ -1,44 +1,24 @@
 require([
-    'jquery', 
-    'underscore', 
-    'prettify', 
-    'splunkjs/ready!', 
+    'jquery',
+    'underscore',
+    'prettify',
+    'splunkjs/ready!',
     'splunkjs/mvc/searchmanager',
     'splunkjs/mvc/datatemplateview'
-    ], 
-    function(
-        $, 
-        _, 
-        prettyPrint,
-        mvc,
-        SearchManager,
-        DataTemplateView
-    ) {
-        new SearchManager({
-            id: 'example-single-search',
-            search: 'index=_internal | stats count | rangemap field=count low=0-3 elevated=3-6 severe=6-10 default=severe',
-            earliest_time: '-15m',
-            latest_time: 'now'
-        });
+], function($, _, prettyPrint, mvc, SearchManager, DataTemplateView) {
 
-        new SearchManager({
-            id: 'search-top-song-downloads',
-            search: '| inputlookup musicdata.csv | search bc_uri=/sync/addtolibrary/* | stats count by track_name | sort count desc | table track_name count',
-            cache: true
-        });
+    new SearchManager({
+        id: 'example-single-search',
+        search: 'index=_internal | stats count | rangemap field=count low=0-3 elevated=3-6 severe=6-10 default=severe',
+        earliest_time: '-15m',
+        latest_time: 'now'
+    });
 
-        new DataTemplateView({
-            id: 'single-search-example',
-            managerid: 'example-single-search',
-            template: '<div class="example-1">Event Count: <%= results[0].count %>. Status: <%= results[0].range %></div>',
-            el: $('#single-search-example')
-        }).render();
+    new DataTemplateView({
+        id: 'single-search-example',
+        managerid: 'example-single-search',
+        template: '<div class="example-1">Event Count: <%= results[0].count %>. Status: <%= results[0].range %></div>',
+        el: $('#single-search-example')
+    }).render();
 
-        new DataTemplateView({
-            id: 'table-top-song-downloads',
-            managerid: 'search-top-song-downloads',
-            template: $('#search-top-song-template').html(),
-            el: $('#search-top-song-table')
-        }).render();
-    }
-);
+});
